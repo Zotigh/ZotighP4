f53ed6c5f43eb7541f346117ff68564a0ec68b75
Lance Zotigh <lancezotighlz@gmail.com>
2020-03-12 10:01:38 -0600

    Add project files.

---
+++ ZotighP4/ZotighP4.cpp
@@ -0,0 +1,17 @@
+// ZotighP4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+/******************************************************
+* Programmer: Lance Zotigh
+* Program: Functions
+* Date: 2/26/2020
+* Purpose: Provide functions needed in AskName AgeFunc
+* project.
+*******************************************************/
+
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
