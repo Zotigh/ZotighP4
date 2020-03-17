458f0c74da3a645281829af7ea12089377be3ea0
Lance Zotigh <lancezotighlz@gmail.com>
2020-03-17 10:21:20 -0600

    Adding more skeletal work meat soon to come.

--- ZotighP4/ZotighP4.cpp
+++ ZotighP4/ZotighP4.cpp
@@ -25,17 +25,26 @@ int main()
 
 	// Header
 	WriteHeader();
+
 	// Begining of play loop
 	do {
-		// Show instructions
+		// Show instructions thought it would be better here since the user if the repeat the program they are show how to use it again
 		WriteInstrictions();
+		
+		// gives user the option to select god bars
+		//selectMetalForBars();
 
 		// Shows list the user can pick from
 		SelectCooperModel();
 
+		// Gets the number of bars
+		SelectNumberOfBars();
 
+		// calculation for weight capacity
+		//CalculateCargoCapacity();
 
-
+		// calculates weight of the bars
+		//CalculateWeightOfBars();
 
 		// cout to ask if user wants to go again
 		cout << "\nWould you like to calculate another? (y/n); ";
